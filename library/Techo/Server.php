7bf93f3aa53e38fa1d67c135e7c8d311252e27ee
UniqueOnly <289211964@qq.com>
2015-02-13 14:29:52 +0800

    add

---
+++ library/Techo/Server.php
@@ -0,0 +1,20 @@
+<?php
+namespace Techo;
+require_once 'Exception.php';
+class Server
+{
+    public static function autoLoad($class)
+    {
+        if (!empty($class)) {
+            $class = ltrim($class, '\\');
+            $file = str_replace(array('_', '\\'), DIRECTORY_SEPARATOR, $class) . '.php';
+            if (file_exists(__LIBRARY__ .$file)) {
+                include $file;
+            } else {
+                throw new \Techo\Exception('The class isn\'t exist');
+            }
+        } else {
+            throw new \Techo\Exception('The class isn\'t exist');
+        }
+    }
+}
