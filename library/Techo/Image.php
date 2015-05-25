c6e0b22a8cb9ce14c02f60d69c589c84306476a2
UniqueOnly <289211964@qq.com>
2015-05-26 00:14:36 +0800

    update

--- library/Techo/Image.php
+++ library/Techo/Image.php
@@ -1,17 +1,41 @@
 <?php
 namespace Techo;
 /**
- * 图像处理外观类
+ * 图像处理类
  * @author unique@hiunique.com
  * @copyright 2015-5-24
  */
-class Image implements \Techo\Image\IImage
+class Image
 {
-    
+    /**
+     * 源图片资源
+     * 
+     * @access protected
+     * @var resource
+     */
     protected $_resource = null;
+    /**
+     * 源图片相关信息
+     * 
+     * @access protected
+     * @var array
+     */
     protected $_imgInfo = null;
+    /**
+     * 源图片路径
+     * 
+     * @access protected
+     * @var string
+     */
     protected $_path = null;
     
+    /**
+     * 构造器
+     * 
+     * @access public
+     * @param string $imgSrc 源图片路径
+     * @throws \Techo\Image\Exception
+     */
     public function __construct($imgSrc = null)
     {
         if ($imgSrc && is_file($imgSrc)) {
@@ -29,6 +53,13 @@ class Image implements \Techo\Image\IImage
         }
     }
     
+    /**
+     * 获取图片数据
+     * 
+     * @access public
+     * @return string
+     * @throws \Techo\Image\Exception
+     */
     public function getData()
     {
         if ($this->_path) {
@@ -39,6 +70,12 @@ class Image implements \Techo\Image\IImage
         
     }
 
+    /**
+     * 输出图片
+     * 
+     * @access public
+     * @throws \Techo\Image\Exception
+     */
     public function output()
     {
         if ($this->_resource) {
@@ -51,14 +88,48 @@ class Image implements \Techo\Image\IImage
         
     }
 
-    public function resize($width, $height, $path = null)
+    /**
+     * 缩放、裁剪图片
+     * 
+     * @access public
+     * @param number $srcWPos 图片裁剪x轴店
+     * @param number $srcHPos 图片裁剪y轴点
+     * @param number $dstWidth 目标图片宽度
+     * @param number $dstHeight 目标图片高度
+     * @param string $path 目标保存路径
+     * @throws \Techo\Image\Exception
+     */
+    public function resize($srcWPos = 0, $srcHPos = 0, $dstWidth = 0, $dstHeight = 0, $path = null)
     {
-        
+        if ($this->_resource) {
+            $srcWPos = $srcWidth > $this->_imgInfo['width'] ? $this->_imgInfo['width'] : $srcWPos;
+            $srcHPos = $srcWidth > $this->_imgInfo['height'] ? $this->_imgInfo['height'] : $srcHPos;
+            $srcWidth = $dstWidth + $srcWPos > $this->_imgInfo['width'] ? $this->_imgInfo['width'] - $srcWPos : $dstWidth;
+            $srcHeight = $dstHeight + $srcHPos > $this->_imgInfo['height'] ? $this->_imgInfo['height'] - $srcHPos : $dstHeight;
+            $dstImg = imagecreatetruecolor($dstWidth, $dstHeight);
+            imagecopyresampled($dstImg, $this->_resource, 0, 0, $srcWPos, $srcHPos, $dstWidth, $dstHeight, $srcWidth, $srcHeight);
+            $dstFunc = 'image' . $this->_imgInfo['type'];
+            if ($path) {
+                header('Content-type:' . $this->_imgInfo['mime']);
+            }
+            $dstFunc($dstImg, $path);
+            imagedestroy($dstImg);
+        } else {
+            throw new \Techo\Image\Exception("No image can be operated!");
+        }
     }
 
+    /**
+     * 设置图片资源
+     * 
+     * @access public
+     * @param string $imageSrc 源图片路径
+     * @throws \Techo\Image\Exception
+     * @return \Techo\Image
+     */
     public function setImage($imageSrc)
     {
-    if ($imgSrc && is_file($imgSrc)) {
+        if ($imgSrc && is_file($imgSrc)) {
             $imgInfo = self::getImgInfo($imgSrc);
             $func = 'imagecreatefrom' . $imgInfo['type'];
             if (function_exists($func)) {
@@ -74,6 +145,11 @@ class Image implements \Techo\Image\IImage
         }
     }
     
+    /**
+     * 销毁图片资源
+     * 
+     * @access public
+     */
     public function destroy()
     {
         if ($this->_resource) {
@@ -84,9 +160,17 @@ class Image implements \Techo\Image\IImage
         }
     }
 
-	public static function getImgInfo($img)
+    /**
+     * 获取图片信息
+     * 
+     * @static
+     * @access public
+     * @param string $imgSrc 图片路径
+     * @return multitype:string unknown Ambigous <>
+     */
+	public static function getImgInfo($imgSrc)
     {
-        $info = getimagesize($img);
+        $info = getimagesize($imgSrc);
         return array(
             'type' => substr(image_type_to_extension($info[2]), 1),
             'mime' => $info['mime'],
@@ -95,16 +179,35 @@ class Image implements \Techo\Image\IImage
         );
     }
     
-    public static function toResize($imgSrc, $width, $height, $path = null)
+    
+    /**
+     * 缩放、裁剪图片
+     * 
+     * @static
+     * @access public
+     * @param string $imgSrc 源图片路径
+     * @param number $srcWPos 图片裁剪x轴店
+     * @param number $srcHPos 图片裁剪y轴点
+     * @param number $dstWidth 目标图片宽度
+     * @param number $dstHeight 目标图片高度
+     * @param string $path 目标保存路径
+     * @throws \Techo\Image\Exception
+     * @return boolean
+     */
+    public static function toResize($imgSrc, $srcWPos = 0, $srcHPos = 0, $dstWidth = 0, $dstHeight = 0, $path = null)
     {
         if (is_file($imgSrc)) {
             $imgInfo = self::getImgInfo($imgSrc);
+            $srcWPos = $srcWidth > $imgInfo['width'] ? $imgInfo['width'] : $srcWPos;
+            $srcHPos = $srcWidth > $imgInfo['height'] ? $imgInfo['height'] : $srcHPos;
+            $srcWidth = $dstWidth + $srcWPos > $imgInfo['width'] ? $imgInfo['width'] - $srcWPos : $dstWidth;
+            $srcHeight = $dstHeight + $srcHPos > $imgInfo['height'] ? $imgInfo['height'] - $srcHPos : $dstHeight;
             $imgType = $imgInfo['type'];
             $createFunc = 'imagecreatefrom' . $imgType;
             if (function_exists($createFunc)) {
                 $img = $createFunc($imgSrc);
-                $dstImg = imagecreatetruecolor($width, $height);
-                imagecopyresampled($dstImg, $dstImg, 0, 0, 0, 0, $width, $height, $imgInfo['width'], $imgInfo['height']);
+                $dstImg = imagecreatetruecolor($dstWidth, $dstHeight);
+                imagecopyresampled($dstImg, $img, 0, 0, $srcWPos, $srcHPos, $dstWidth, $dstHeight, $srcWidth, $srcHeight);
                 $dstFunc = 'image' . $imgType;
                 if ($path) {
                     header('Content-type:' . $imgInfo['mime']);
@@ -119,6 +222,16 @@ class Image implements \Techo\Image\IImage
         } else {
             throw new \Techo\Image\Exception("The image isn't exist!");
         }  
+    }
+    
+    /**
+     * 析构函数
+     * 
+     * @access public
+     */
+    public function __destruct()
+    {
+        $this->destroy();
     }
     	
 }
