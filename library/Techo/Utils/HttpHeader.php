0917a168df36ff2aa34ff602aeb603ea7788d5ba
UniqueOnly <289211964@qq.com>
2015-06-28 22:27:01 +0800

    update

--- library/Techo/Utils/HttpHeader.php
+++ library/Techo/Utils/HttpHeader.php
@@ -8,19 +8,24 @@ class HttpHeader
      * 
      * @static
      * @access public
-     * @param number $timeout 秒数(小于或等于0秒表示禁止缓存)
+     * @param number $timeout 秒数(等于0秒表示禁止缓存,小于0不做操作)
      */
     public static function expire($timeout = 0)
     {
-        $expire = time() + $timeout;
-        $expireGMT = gmdate('D, d M Y H:i:s', $expire) . ' GMT';
-        header ('Last-Modified: ' . $expireGMT);
-        header ('Expires: ' . $expireGMT);
-        header ('Cache-Control: max-age=' . $timeout);
-        header("Pragma: cache");
-        if ($timeout < 0) {
-            header('Cache-Control: no-cache, must-revalidate');
-            header('Pramga: no-cache');
+        if ($timeout >= 0) {
+            $expire = time() + $timeout;
+            $expireGMT = gmdate('D, d M Y H:i:s', $expire) . ' GMT';
+            if ($timeout == 0) {
+                header('Cache-Control: no-cache');
+                header('Cache-Control ：no-store');
+                header('Pramga: no-cache');
+                header('Expires：1L');
+            } else {
+                header('Last-Modified: ' . $expireGMT);
+                header('Expires: ' . $expireGMT);
+                header('Cache-Control: max-age=' . $timeout);
+                header("Pragma: cache");
+            }
         }
     }
 }
